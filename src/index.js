45d858a3b3eb7de46d75bf96816df69001b66800
Rvouzz <febriadi060204@gmail.com>
2024-05-28 15:21:02 +0700

    Express

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ const app = express();
 const userRoutes = require("./routes/user");
 const middlewareLogReq = require("./middleware/logs");
 
-app.use("/user", userRoutes);
-
-app.use(middlewareLogReq);
 app.use(express.json());
+app.use(middlewareLogReq);
+app.use("/user", userRoutes);
 
 app.listen(4000, () => {
   console.log("Server is running on port 4000");
