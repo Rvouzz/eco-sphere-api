782966fd796cea8831a5e1b0a0f3738fa1c22689
Rvouzz <febriadi060204@gmail.com>
2024-06-21 18:51:14 +0700

    Google Auth

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,36 +1,39 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const UserModel = require('../models/user');
-const { secretKey } = require('./database');
 
 passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/api/auth/google/callback'
-  },
-  async (token, tokenSecret, profile, done) => {
-    try {
-      const [existingUser] = await UserModel.getUserByEmail(profile.emails[0].value);
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: '/api/auth/google/callback'
+},
+async (token, tokenSecret, profile, done) => {
+  try {
+    console.log('Google profile:', profile);
+    
+    const [existingUser] = await UserModel.getUserByEmail(profile.emails[0].value);
 
-      if (existingUser.length > 0) {
-        return done(null, existingUser[0]);
-      }
-
-      const newUser = {
-        email: profile.emails[0].value,
-        nama_depan: profile.name.givenName,
-        nama_belakang: profile.name.familyName,
-        img_profile: profile.photos[0].value,
-        googleid: profile.id
-      };
-
-      const [createdUser] = await UserModel.createNewUser(newUser, profile.name.givenName, profile.name.familyName);
-      return done(null, createdUser[0]);
-    } catch (err) {
-      return done(err, false);
+    if (existingUser.length > 0) {
+      console.log('User exists:', existingUser[0]);
+      return done(null, existingUser[0]);
     }
+
+    const newUser = {
+      email: profile.emails[0].value,
+      nama_depan: profile.name.givenName,
+      nama_belakang: profile.name.familyName,
+      img_profile: profile.photos[0].value,
+      googleid: profile.id
+    };
+
+    const [createdUser] = await UserModel.createNewUser(newUser, profile.name.givenName, profile.name.familyName);
+    console.log('New user created:', createdUser[0]);
+    return done(null, createdUser[0]);
+  } catch (err) {
+    console.error('Error in Google strategy:', err);
+    return done(err, false);
   }
-));
+}));
 
 passport.serializeUser((user, done) => {
   done(null, user.id_user);
@@ -38,13 +41,17 @@ passport.serializeUser((user, done) => {
 
 passport.deserializeUser(async (id, done) => {
   try {
+    console.log(`Deserializing user with ID: ${id}`);
     const [user] = await UserModel.getUserById(id);
     if (user.length > 0) {
+      console.log('User found:', user[0]);
       done(null, user[0]);
     } else {
-      done(new Error("User not found"), null);
+      console.error('User not found:', id);
+      done(new Error('User not found'), null);
     }
   } catch (err) {
+    console.error('Error deserializing user:', err);
     done(err, false);
   }
 });
